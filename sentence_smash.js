a92dca2026bc1213424f7dde2cce70b1079c50a3
rubisanto <rubisanto06@gmail.com>
2023-06-14 11:46:10 +0200

    first commit

---
+++ sentence_smash.js
@@ -0,0 +1,6 @@
+//Write a function that takes an array of words and smashes them together into a sentence and returns the sentence
+
+function smash(words) {
+  "use strict";
+  return words.join(" ");
+}
